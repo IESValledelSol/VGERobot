d55a18fa89d42c2e681820a135bb19969304fc89
IES Valle del Sol (Álora, Málaga) <131544207+IESValledelSol@users.noreply.github.com>
2023-04-23 14:31:59 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,28 @@
 radio.onReceivedValue(function (name, value) {
+    if (name == "Q") {
+        if (value == 0) {
+            maqueen.servoRun(maqueen.Servos.S1, 90)
+            angulocamara = 90
+        }
+    }
     if (name == "D") {
-        if (value >= 0) {
-            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
-            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-            if (value == 0) {
-                tiraled.showColor(neopixel.rgb(255, 255, 255))
-            }
-            if (value == 1) {
-                tiraled.showColor(neopixel.rgb(255, 0, 0))
-            }
-            if (value == 2) {
-                tiraled.showColor(neopixel.rgb(0, 255, 0))
-            }
-            if (value == 3) {
-                tiraled.showColor(neopixel.rgb(0, 0, 255))
+        if (value == 0) {
+            anguloherramienta = anguloherramienta - 1
+            if (anguloherramienta >= minanguloherramienta) {
+                maqueen.servoRun(maqueen.Servos.S2, anguloherramienta)
+            } else {
+                anguloherramienta = minanguloherramienta
             }
         }
     }
     if (name == "E") {
         if (value == 0) {
-            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
-            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-            tiraled.showColor(neopixel.rgb(0, 0, 0))
+            anguloherramienta = anguloherramienta + 1
+            if (anguloherramienta <= maxanguloherramienta) {
+                maqueen.servoRun(maqueen.Servos.S2, anguloherramienta)
+            } else {
+                anguloherramienta = maxanguloherramienta
+            }
         }
     }
     if (name == "F") {
@@ -33,14 +34,6 @@ radio.onReceivedValue(function (name, value) {
                 angulocamara = 0
             }
         }
-        if (value == 1) {
-            anguloherramienta = anguloherramienta - 1
-            if (anguloherramienta >= 5) {
-                maqueen.servoRun(maqueen.Servos.S2, anguloherramienta)
-            } else {
-                anguloherramienta = 5
-            }
-        }
     }
     if (name == "C") {
         if (value == 0) {
@@ -49,14 +42,6 @@ radio.onReceivedValue(function (name, value) {
                 maqueen.servoRun(maqueen.Servos.S1, angulocamara)
             } else {
                 angulocamara = 180
-            }
-        }
-        if (value == 1) {
-            anguloherramienta = anguloherramienta + 1
-            if (anguloherramienta <= 90) {
-                maqueen.servoRun(maqueen.Servos.S2, anguloherramienta)
-            } else {
-                anguloherramienta = 90
             }
         }
     }
@@ -71,28 +56,36 @@ radio.onReceivedValue(function (name, value) {
     }
     if (yvalor >= 750 && (xvalor > 250 && xvalor < 750)) {
         ultimaorden = "JoystickAvanza"
+        tiraled.showColor(neopixel.rgb(255, 255, 255))
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
     } else if (ultimaorden == "JoystickAvanza") {
+        tiraled.showColor(neopixel.rgb(0, 0, 0))
         maqueen.motorStop(maqueen.Motors.All)
     }
     if (xvalor >= 750 && (yvalor > 250 && yvalor < 750)) {
         ultimaorden = "JoystickDerecha"
+        tiraled.showColor(neopixel.rgb(0, 255, 0))
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, velocidad)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, velocidad)
     } else if (ultimaorden == "JoystickDerecha") {
+        tiraled.showColor(neopixel.rgb(0, 0, 0))
         maqueen.motorStop(maqueen.Motors.All)
     }
     if (yvalor <= 250 && (xvalor > 250 && xvalor < 750)) {
         ultimaorden = "JoystickRetrocede"
+        tiraled.showColor(neopixel.rgb(255, 0, 0))
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, velocidad)
     } else if (ultimaorden == "JoystickRetrocede") {
+        tiraled.showColor(neopixel.rgb(0, 0, 0))
         maqueen.motorStop(maqueen.Motors.All)
     }
     if (xvalor <= 250 && (yvalor > 250 && yvalor < 750)) {
         ultimaorden = "JoystickIzquierda"
+        tiraled.showColor(neopixel.rgb(0, 0, 255))
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velocidad)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, velocidad)
     } else if (ultimaorden == "JoystickIzquierda") {
+        tiraled.showColor(neopixel.rgb(0, 0, 0))
         maqueen.motorStop(maqueen.Motors.All)
     }
 })
@@ -103,11 +96,15 @@ let tiraled: neopixel.Strip = null
 let anguloherramienta = 0
 let angulocamara = 0
 let velocidad = 0
+let minanguloherramienta = 0
+let maxanguloherramienta = 0
 basic.showIcon(IconNames.Yes)
 radio.setGroup(23)
+maxanguloherramienta = 40
+minanguloherramienta = 5
 velocidad = 20
 angulocamara = 90
-anguloherramienta = 5
+anguloherramienta = minanguloherramienta
 maqueen.servoRun(maqueen.Servos.S1, angulocamara)
 maqueen.servoRun(maqueen.Servos.S2, anguloherramienta)
 tiraled = neopixel.create(DigitalPin.P1, 8, NeoPixelMode.RGB)
